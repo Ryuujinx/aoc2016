29b7fba7cf0fcb24999ef57b3bd2db364b6bb9db
Ryuujinx <admin@failverse.com>
2016-12-20 21:54:39 -0600

    Herpderp

--- day7/day7-part2.rb
+++ day7/day7-part2.rb
@@ -38,13 +38,13 @@ File.open("day7-input", "r") do |f|
 		0.upto(address["segments"]) do |iter|
 			arr=Array.new
 			address["s#{iter}"].chars.each do |chr|
+				arr << chr
 				if arr.count == 3
 					if arr[0] != arr[1] && arr[0] == arr[2]
 						aba << arr[0] + arr[1] + arr[2]
 					end
 					arr.shift
 				end
-				arr << chr
 			end
 		end
 		0.upto(address["brackets"]) do |iter|
